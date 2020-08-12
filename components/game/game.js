b2ced20143e81f5490221ed442b87311dbdc3a7a
Gregor Hann <gregor585@gmail.com>
2020-08-12 14:42:38 +0200

    Add player turn and todos

--- components/game/game.js
+++ components/game/game.js
@@ -7,6 +7,7 @@ class Game {
       columns: 10,
       rows: 10,
     });
+    this.playerOnTurn = null;
   }
 
   addPlayer(player) {
@@ -22,9 +23,27 @@ class Game {
   }
 
   startGame() {
+    // position players to starting space
     for (const player of this.players) {
       this.movePlayer(player, 0);
     }
+
+    // TODO: move to a game loop
+    // start player turn
+    this.startNextPlayerTurn();
+  }
+
+  startNextPlayerTurn() {
+    if (!this.playerOnTurn) {
+      // TODO: should we use `this.players` directly or `this.getPlayers()`?
+      this.playerOnTurn = this.players[0];
+    }
+
+    // TODO: assign next player in line
+  }
+
+  getPlayerOnTurn() {
+    return this.playerOnTurn;
   }
 }
 
--- components/game/game.test.js
+++ components/game/game.test.js
@@ -3,10 +3,9 @@ import Player from '../../models/player/player';
 
 describe('Game', () => {
   const game = new Game();
-  const players = [{ name: 'Player 1' }, { name: 'Player 2' }];
 
-  game.addPlayer(Player(players[0]));
-  game.addPlayer(Player(players[1]));
+  game.addPlayer(Player({ name: 'Player 1' }));
+  game.addPlayer(Player({ name: 'Player 2' }));
 
   test('starts with players on initial position', () => {
     game.startGame();
@@ -18,4 +17,19 @@ describe('Game', () => {
       ])
     );
   });
+
+  test('turn starts with first player', () => {
+    game.startGame();
+
+    expect(game.getPlayerOnTurn()).toStrictEqual({
+      name: 'Player 1',
+      position: 0,
+    });
+  });
+
+  test.todo('moves player to correct space after dice rolls onto normal field');
+
+  test.todo('moves player to correct space after dice rolls onto snake field');
+
+  test.todo('moves player to correct space after dice rolls onto ladder field');
 });
